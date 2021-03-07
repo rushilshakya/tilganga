fd431b9e547b62bbe313f73a6147dd6c200b30b9
rushilshakya <rushil.shakya@gmail.com>
2021-03-07 11:08:32 +0545

    feat: socket io to allow CORS

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
 const cors = require("cors");
+const io = require("socket.io")(http, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
 const port = process.env.PORT || 3000;
 
 app.use(cors());
